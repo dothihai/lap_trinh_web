986a1049596f718c2d54af223c94c53fbc651df6
dothihai <dothihaia10k50@gmail.com>
2018-09-25 11:20:55 +0700

    first commit

---
+++ lab2/ex9.php
@@ -0,0 +1,11 @@
+<?php
+ $ten = $_POST['ten'];
+ $lop = $_POST['lop'];
+ $truong = $_POST['truong'];
+ $gioitinh = $_POST['gioitinh'];
+ $sothich = $_POST['sothich'];
+ echo "<br> Xin chao, ".$ten;
+ echo "<br> ".$lop.", ".$truong;
+ echo "<br> Gioi tinh: ".$gioitinh;
+ echo "<br> So thich: ".$sothich;
+?>
